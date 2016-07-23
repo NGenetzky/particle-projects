a073a24e2dbb4ade53ceb26500f8807a84b6984a
Nate Genetzky <nathan.genetzky@daktronics.com>
2016-07-23 18:05:17 +0000

    board-rgb-demo: Updated scripts

--- board-rgb-demo/compile_firmware.bash
+++ board-rgb-demo/compile_firmware.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 type=${1?"Specify the device type as first arg."}
-particle compile $type ./ --saveTo firmware.bin
+particle compile $type ./firmware --saveTo "$type""_firmware.bin"
