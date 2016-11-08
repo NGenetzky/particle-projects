fff94f39fc441287479f788f6e84bc1e00bf84b2
Nate Genetzky <nathan.genetzky@daktronics.com>
2016-11-08 07:17:56 +0000

    i2c: Added read/write to master_utility.

--- firmware/i2c/i2c_master_utility.cpp
+++ firmware/i2c/i2c_master_utility.cpp
@@ -4,6 +4,7 @@
 extern void info(const char * name, const char * data);
 
 static bool is_master = false;
+static uint8_t last_slave = 0;
 
 namespace i2c_master {
 
@@ -20,19 +21,57 @@ namespace i2c_master {
         return 0;
     }
 
+    int open(uint8_t new_slave){
+        auto old = last_slave;
+        last_slave = new_slave;
+        return old;
+    }
+
+    int write(std::vector<uint8_t> d){
+        auto len = d.size();
+        if( 32 < len ){
+            len = 32;
+            info("i2c_master", "Write is limited to first 32 bytes.");
+        }
+        auto data = d.data();
+        Wire.beginTransmission(last_slave); // transmit to slave device #4
+        Wire.write(data, len);             // send data from vector.
+        return Wire.endTransmission();    // stop transmitting
+    }
+
+    std::vector<uint8_t> read(unsigned bytes_requested){
+        std::vector<uint8_t> data;
+        auto requested = bytes_requested; 
+        if( 32 < requested ){
+            requested = 32;
+            info("i2c_master", "Read is limited to first 32 bytes.");
+        }
+
+        auto received = Wire.requestFrom(last_slave, requested);
+        // slave may send less than requested
+        while(Wire.available()) {
+            char c = Wire.read();    // receive a byte as character
+            data.push_back(c);
+        }
+        if(requested != data.size()){
+            info("i2c_master", "Read did not receive expected number of bytes.");
+        }
+        return data;
+    }
+
     void print_end_transmission_rv(int ret_code){
-    char error_string[65];
-    switch(ret_code){
-        case 0: sprintf(error_string,"0: success"); break;
-        case 1: sprintf(error_string,"1: busy timeout upon entering endTransmission()"); break;
-        case 2: sprintf(error_string,"2: START bit generation timeout"); break;
-        case 3: sprintf(error_string,"3: end of address transmission timeout"); break;
-        case 4: sprintf(error_string,"4: data byte transfer timeout"); break;
-        case 5: sprintf(error_string,"5: data byte transfer succeeded, busy timeout immediately after"); break;
-    }
-    if(ret_code != 0){
-        info("I2C_END_TRANSMISSION", error_string);
-    }
+        char error_string[65];
+        switch(ret_code){
+            case 0: sprintf(error_string,"0: success"); break;
+            case 1: sprintf(error_string,"1: busy timeout upon entering endTransmission()"); break;
+            case 2: sprintf(error_string,"2: START bit generation timeout"); break;
+            case 3: sprintf(error_string,"3: end of address transmission timeout"); break;
+            case 4: sprintf(error_string,"4: data byte transfer timeout"); break;
+            case 5: sprintf(error_string,"5: data byte transfer succeeded, busy timeout immediately after"); break;
+        }
+        if(ret_code != 0){
+            info("I2C_END_TRANSMISSION", error_string);
+        }
     }
 
     int scan(){
@@ -43,7 +82,7 @@ namespace i2c_master {
         
         if ( Wire.isEnabled() ) {
             //info("I2C.setup", "reset;begin(Master)");
-            #warning Assumes that device is acting as master.
+            // #warning Assumes that device is acting as master.
             if(!is_master){
                 setup(); // Must be master to scan.
             }
--- firmware/i2c/i2c_master_utility.h
+++ firmware/i2c/i2c_master_utility.h
@@ -1,7 +1,12 @@
 #pragma once
+#include "application.h"
+#include <vector>
 
 namespace i2c_master {
     int setup();
+    int open(uint8_t new_slave);
+    int write(std::vector<uint8_t> d);
+    std::vector<uint8_t> read(unsigned bytes_requested);
     void print_end_transmission_rv(int ret_code);
     int scan();
 }
