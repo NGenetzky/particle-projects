53420d1c5a3c8db01f2b94a48757fbd663a6b1ef
Nate Genetzky <nathan.genetzky@daktronics.com>
2016-11-08 04:53:10 +0000

    template: Added vimrc for :make and scripts changed to work with it.

--- scripts/compile_firmware.bash
+++ scripts/compile_firmware.bash
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 type=${1?"Specify the device type as first arg."}
-particle compile $type ./firmware --saveTo "$type""_firmware.bin"
+cd ./firmware/
+particle compile $type ./ --saveTo "../$type""_firmware.bin"
--- scripts/flash_device.bash
+++ scripts/flash_device.bash
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 device=${1?"Specify the device as first arg."}
-particle flash $device ./firmware
+cd ./firmware/
+particle flash $device ./
