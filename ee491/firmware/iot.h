223c59457b10cc5f98690907a4713b7c87f90c22
Nate Genetzky <nathan.genetzky@daktronics.com>
2016-12-21 06:41:47 +0000

    tinker: Using DR and DW from app with my program.

--- ee491/firmware/iot.h
+++ ee491/firmware/iot.h
@@ -94,6 +94,10 @@ class DigitalPort {
         for( auto &d : this->dpins ){
             d.setup();
         }
+
+        Particle.function("digitalread", &DigitalPort::tinkerDigitalRead, this);
+        Particle.function("digitalwrite", &DigitalPort::tinkerDigitalWrite, this);
+
         return 0;
     }
     int add(DigitalPin o){this->dpins.push_back(o); return 0; } ;
@@ -114,6 +118,105 @@ class DigitalPort {
             this->dpins[i].set( bits[i] );
         }
         return 0;
+    }
+
+    int digitalRead(int pin){
+        return this->dpins[pin].get();
+    }
+
+    int digitalWrite(int pin, bool value){
+        return this->dpins[pin].set(value);
+    }
+    /*******************************************************************************
+    * Function Name  : tinkerDigitalRead
+    * Description    : Reads the digital value of a given pin
+    * Input          : Pin
+    * Output         : None.
+    * Return         : Value of the pin (0 or 1) in INT type
+                        Returns a negative number on failure
+    *******************************************************************************/
+    int tinkerDigitalRead(String pin)
+    {
+        //convert ascii to integer
+        int pinNumber = pin.charAt(1) - '0';
+        //Sanity check to see if the pin numbers are within limits
+        if (pinNumber < 0 || pinNumber > 7) return -1;
+
+        if(pin.startsWith("D"))
+        {
+            // pinMode(pinNumber, INPUT_PULLDOWN);
+            return this->digitalRead(pinNumber);
+        }
+        else if (pin.startsWith("A"))
+        {
+            // pinMode(pinNumber+10, INPUT_PULLDOWN);
+            return this->digitalRead(pinNumber+8);
+        }
+#if Wiring_Cellular
+        else if (pin.startsWith("B"))
+        {
+            if (pinNumber > 5) return -3;
+            // pinMode(pinNumber+24, INPUT_PULLDOWN);
+            return this->digitalRead(pinNumber+16);
+        }
+        else if (pin.startsWith("C"))
+        {
+            if (pinNumber > 5) return -4;
+            // pinMode(pinNumber+30, INPUT_PULLDOWN);
+            return this->digitalRead(pinNumber+24);
+        }
+#endif
+        return -2;
+    }
+
+    /*******************************************************************************
+    * Function Name  : tinkerDigitalWrite
+    * Description    : Sets the specified pin HIGH or LOW
+    * Input          : Pin and value
+    * Output         : None.
+    * Return         : 1 on success and a negative number on failure
+    *******************************************************************************/
+    int tinkerDigitalWrite(String command)
+    {
+        bool value = 0;
+        //convert ascii to integer
+        int pinNumber = command.charAt(1) - '0';
+        //Sanity check to see if the pin numbers are within limits
+        if (pinNumber < 0 || pinNumber > 7) return -1;
+
+        if(command.substring(3,7) == "HIGH") value = 1;
+        else if(command.substring(3,6) == "LOW") value = 0;
+        else return -2;
+
+        if(command.startsWith("D"))
+        {
+            // pinMode(pinNumber, OUTPUT);
+            this->digitalWrite(pinNumber, value);
+            return 1;
+        }
+        else if(command.startsWith("A"))
+        {
+            // pinMode(pinNumber+10, OUTPUT);
+            this->digitalWrite(pinNumber+8, value);
+            return 1;
+        }
+#if Wiring_Cellular
+        else if(command.startsWith("B"))
+        {
+            if (pinNumber > 5) return -4;
+            // pinMode(pinNumber+24, OUTPUT);
+            this->digitalWrite(pinNumber+16, value);
+            return 1;
+        }
+        else if(command.startsWith("C"))
+        {
+            if (pinNumber > 5) return -5;
+            // pinMode(pinNumber+30, OUTPUT);
+            this->digitalWrite(pinNumber+24, value);
+            return 1;
+        }
+#endif
+        else return -3;
     }
     
     private:
@@ -326,8 +429,8 @@ class App {
 
     bool setup_PF_tinker(){
         //Register all the Tinker functions
-        Particle.function("digitalread", &App::PF_cin, this);
-        Particle.function("digitalwrite", &App::PF_cin, this);
+        // Particle.function("digitalread", &App::PF_cin, this);
+        // Particle.function("digitalwrite", &App::PF_cin, this);
 
         Particle.function("analogread", &App::PF_cin, this);
         Particle.function("analogwrite", &App::PF_cin, this);
