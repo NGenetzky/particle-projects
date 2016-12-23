b78d677db947e19819357d3cd9aa47b09fcbb2f6
Nate Genetzky <nathan.genetzky@daktronics.com>
2016-12-23 16:53:04 +0000

    scripts: Support either project dir or project/firmware.

--- scripts/compile_firmware.bash
+++ scripts/compile_firmware.bash
@@ -2,7 +2,10 @@
 type=${1?"Specify the device type as first arg."}
 
 firmware_dir=./firmware
-[ -d $firmware_dir ] || { echo "Can't find $firmware_dir"; exit 1; }
+if [ -d $firmware_dir ]; then
 cd $firmware_dir
+fi
+# { echo "Can't find $firmware_dir"; exit 1; }
+
 
 particle compile $type ./ --saveTo "../$type""_firmware.bin"
--- scripts/flash_device.bash
+++ scripts/flash_device.bash
@@ -2,6 +2,9 @@
 device=${1?"Specify the device as first arg."}
 
 firmware_dir=./firmware
-[ -d $firmware_dir ] || { echo "Can't find $firmware_dir"; exit 1; }
+if [ -d $firmware_dir ]; then
+cd $firmware_dir
+fi
+#  [ -d $firmware_dir ] || { echo "Can't find $firmware_dir"; exit 1; }
 
 particle flash $device ./
