005139c318374e054e4fb61a67d2af588447477c
Nate Genetzky <nathan.genetzky@daktronics.com>
2016-12-23 19:45:08 +0000

    test: Combined scripts for testing.

---
+++ ee491/test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+flash_device() {
+    flash_device.bash Logan
+    sleep 10s
+}
+
+test_digitalport(){
+        particle call Logan "get" ""
+
+        particle call Logan "set" "15"
+
+        particle call Logan "get" ""
+
+        particle call Logan "set" "0"
+
+        particle call Logan "get" ""
+}
+
+test_cin_stdout(){
+    particle call Logan cin "\$(set 15)"
+    # particle get Logan stdin
+    particle get Logan stdout
+
+    particle call Logan cin "\$(get)"
+    # particle get Logan stdin
+    particle get Logan stdout
+}
+
+# Main
+set -x
+
+# flash_device
+# test_digitalport
+test_cin_stdout
