1fbc3827aeff84bfe3a721f8e9034ba1a643c710
ngenetzky <n@genetzky.us>
2017-04-04 19:38:22 +0000

    App: Removed heavy use of macros for App*.h

--- ee491/firmware/App.h
+++ ee491/firmware/App.h
@@ -2,39 +2,52 @@
 #include "application.h" // Required for Particle.
 
 #define DIGITALPORT_EN 1
-#include "AppDigitalPort.h"
+#include "DigitalPort.h"
 
 #define STDIN_EN 1
-#include "AppStdin.h"
-
 #define STDOUT_EN 1
-#include "AppStdout.h"
+#include "File.h"
 
 #define TINKER_EN 1
-#include "AppTinker.h"
+#include "Tinker.h"
 
 #define REGISTERBANK_EN 1
-#include "AppRegisterBank.h"
+#include "RegisterBank.h"
 
 namespace iot {
-
 class App {
     public:
-
+// *****************************************************************************
+// Constructors
+// *****************************************************************************
     App(const char *HELP): HELP(HELP)
     { };
 
 #if DIGITALPORT_EN == 1
-    App( const char *HELP, DIGITALPORT_TYPE digitalport )
-        : HELP( HELP ), DIGITALPORT_VAR( digitalport ){};
+    App( const char *HELP, DigitalPort digitalport )
+        : HELP( HELP ), dport( digitalport ){};
 #endif
 
+// *****************************************************************************
+// Main (setup & loop)
+// *****************************************************************************
     int setup(){
         this->setup_PV_help();
-        TINKER_SETUP
-        DIGITALPORT_SETUP
-        STDIN_SETUP
-        STDOUT_SETUP
+#if DIGITALPORT_EN == 1
+        this->dport.setup();
+#endif
+        
+#if 1 == STDIN_EN
+        std_in.setup();
+        std_in.setup_PV_data( "stdin" );
+        std_in.setup_PF_in( "cin" );
+#endif
+
+#if 1 == STDOUT_EN
+        std_out.setup();
+        std_out.setup_PV_data( "stdout" );
+#endif
+        
         return 0;
     }
 
@@ -46,17 +59,71 @@ class App {
 
     bool setup_PV_help(){ return Particle.variable("help", this->HELP); }
 
+// *****************************************************************************
+// Variables
+// *****************************************************************************
     private:
         const char *HELP;
         int loop_count = 0;
 
+// *****************************************************************************
+// Optional Components:
+// *****************************************************************************
     public:
+    
+#if 1 == TINKER_EN
+    iot::Tinker tinker;
+    
+    int add( TinkerFunction f ) {
+        this->tinker.add( f );
+        return 0;
+    };
+    int add( iot::Tinker tinker ) {
+        this->tinker = tinker;
+        return 0;
+    };
+    bool setup_PF_tinker() { return this->tinker.setup_PF_tinker(); }
+#endif
 
-    TINKER_APP
-    DIGITALPORT_APP
-    REGISTERBANK_APP
-    STDIN_APP
-    STDOUT_APP
+#if 1 == DIGITALPORT_EN
+    DigitalPort dport;
+    
+    int add( DigitalPin dpin ) {
+        this->dport.add( dpin );
+        return 0;
+    };
+    int add( DigitalPort dport ) {
+        this->dport = dport;
+        return 0;
+    };
+    bool setup_PF_set() { return this->dport.setup_PF_set(); }
+    bool setup_PF_get() { return this->dport.setup_PF_get(); }
+    bool setup_PF_digitalread() { return this->dport.setup_PF_digitalread(); }
+    bool setup_PF_digitalwrite() { return this->dport.setup_PF_digitalwrite(); }
+#endif
+    
+#if 1 == REGISTERBANK_EN
+    iot::RegisterBank regs;     
+    
+    int add( Register dpin ){                                       
+        this->regs.add( dpin ); 
+        return 0;                           
+    };                                      
+    int add( RegisterBank x ){                                       
+        this->regs = x;         
+        return 0;                           
+    };                                      
+    bool setup_PF_reg() { return this->regs.setup_PF_reg(); }
+#endif
+    
+#if 1 == STDIN_EN
+    iot::File std_in;
+#endif
+
+#if 1 == STDOUT_EN
+    iot::File std_out;
+#endif
+
 };
 
 // iot
