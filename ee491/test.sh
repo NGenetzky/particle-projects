38e3250b63e9672e67e313705cc6f239a124119f
ngenetzky <N@Genetzky.us>
2017-04-08 02:10:55 +0000

    test: allow to work with configureable device.

--- ee491/test.sh
+++ ee491/test.sh
@@ -1,52 +1,79 @@
 #!/usr/bin/env bash
 
+################################################################################
+# Utility Functions
+################################################################################
 flash_device() {
-    flash_device.bash Logan
-    sleep 10s
+    device=$1
+    
+    firmware_dir=./firmware
+    if [ -d $firmware_dir ]; then
+        cd $firmware_dir
+    fi
+    #  [ -d $firmware_dir ] || { echo "Can't find $firmware_dir"; exit 1; }
+    
+    particle flash $device ./
 }
 
+################################################################################
+# Functions to test particular functionality
+################################################################################
 test_digitalport(){
-        particle call Logan "get" ""
+        particle call $device "get" ""
 
-        particle call Logan "set" "15"
+        particle call $device "set" "15"
 
-        particle call Logan "get" ""
+        particle call $device "get" ""
 
-        particle call Logan "set" "0"
+        particle call $device "set" "0"
 
-        particle call Logan "get" ""
+        particle call $device "get" ""
 }
 
 test_cin_stdout(){
-    particle call Logan cin "\$(reg 0)"
-    particle get Logan stdout
+    particle call $device cin "\$(reg 0)"
+    particle get $device stdout
     
-    particle call Logan cin "\$(DW D7=HIGH)"
-    particle get Logan stdout
+    particle call $device cin "\$(DW D7=HIGH)"
+    particle get $device stdout
 
-    particle call Logan cin "\$(DR D7)"
-    particle get Logan stdout
+    particle call $device cin "\$(DR D7)"
+    particle get $device stdout
 
-    particle call Logan cin "\$(DR D4)"
-    particle get Logan stdout
+    particle call $device cin "\$(DR D4)"
+    particle get $device stdout
 
-    particle call Logan cin "\$(set 15)"
-    particle get Logan stdout
+    particle call $device cin "\$(set 15)"
+    particle get $device stdout
 
-    particle call Logan cin "\$(get)"
-    particle get Logan stdout
+    particle call $device cin "\$(get)"
+    particle get $device stdout
 
-    particle call Logan cin "\$(get)"
-    particle get Logan stdout
+    particle call $device cin "\$(get)"
+    particle get $device stdout
     
-    particle call Logan cin "\$(reg 0)"
-    particle get Logan stdout
+    particle call $device cin "\$(reg 0)"
+    particle get $device stdout
 }
 
+################################################################################
 # Main
+################################################################################
+
+################################################################################
+# Main.Setup
+################################################################################
+device=${1?"Specify the device as first arg."}
+
 set -x
 set -e
 
-flash_device
+################################################################################
+# Parse Arguments
+################################################################################
+
+flash_device $device
+sleep 10s
+
 # test_digitalport
 test_cin_stdout
