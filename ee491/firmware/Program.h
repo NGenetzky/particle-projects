98b7629031a7f3a94be4374f62543cda1bdb2ea3
ngenetzky <N@Genetzky.us>
2017-04-08 04:50:00 +0000

    Program: Started idea. Could be replace "file_pipe.h"

---
+++ ee491/firmware/Program.h
@@ -0,0 +1,20 @@
+
+// #pragma once
+// #include "File.h"
+// namespace {
+
+// struct Program {
+    
+//     void run(){
+        
+//     }
+    
+//     bool run_request=false;
+//     std::vector<char> args;
+//     std::vector<char> data;
+//     iot::File *file_in;
+//     iot::File *file_out;
+// };
+
+// }
+
