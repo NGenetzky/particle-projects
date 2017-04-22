8199433bb3301622d3330eed6153570dec86e587
ngenetzky <N@Genetzky.us>
2017-04-22 23:16:45 +0000

    spi_slave: Added publish output (had only serial)

--- projects/spi_slave/src/spi_slave.cpp
+++ projects/spi_slave/src/spi_slave.cpp
@@ -1,6 +1,9 @@
 
 #include "application.h"
 
+// #define SERIAL_EN
+// #define PUBLISH_EN
+
 // SPI slave example
 static uint8_t rx_buffer[64];
 static uint8_t tx_buffer[64];
@@ -18,9 +21,12 @@ void onSelect(uint8_t state) {
 
 /* executes once at startup */
 void setup() {
+#ifdef SERIAL_EN
     Serial.begin(9600);
-    for (int i = 0; i < sizeof(tx_buffer); i++)
+#endif
+    for (int i = 0; i < sizeof(tx_buffer); i++){
       tx_buffer[i] = (uint8_t)i;
+    }
     SPI.onSelect(onSelect);
     SPI.begin(SPI_MODE_SLAVE, A2);
 }
@@ -28,19 +34,37 @@ void setup() {
 /* executes continuously after setup() runs */
 void loop() {
     while (1) {
-        while(select_state == 0);
+        while(select_state == 0){};
         select_state = 0;
 
         transfer_state = 0;
         SPI.transfer(tx_buffer, rx_buffer, sizeof(rx_buffer), onTransferFinished);
-        while(transfer_state == 0);
+        while(transfer_state == 0){};
         if (SPI.available() > 0) {
+            
+#ifdef PUBLISH_EN
+            Particle.publish("spi_rx_len",
+                String::format("Received %d bytes", SPI.available())
+                );
+            String received;
+            received.reserve(64+1);
+            for (int i = 0; i < SPI.available(); i++) {
+                received += char(rx_buffer[i]);
+                // received += String::format("%02x ", rx_buffer[i]);
+            }
+            Particle.publish("spi_rx_asci", received);
+            // Particle.publish("spi_rx_data", received);
+#endif
+
+#ifdef SERIAL_EN
             Serial.printf("Received %d bytes", SPI.available());
             Serial.println();
             for (int i = 0; i < SPI.available(); i++) {
                 Serial.printf("%02x ", rx_buffer[i]);
             }
             Serial.println();
+#endif
+
         }
     }
 }
