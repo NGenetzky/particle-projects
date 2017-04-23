4c997191d1aca786ce9ab60370ad6f349a34e4d3
ngenetzky <N@Genetzky.us>
2017-04-23 00:04:02 +0000

    spi_slave: Reformatting and fixed while(1) in loop.

--- projects/spi_slave/src/spi_slave.cpp
+++ projects/spi_slave/src/spi_slave.cpp
@@ -2,21 +2,62 @@
 #include "application.h"
 
 // #define SERIAL_EN
-// #define PUBLISH_EN
+#define PUBLISH_EN
+
+enum class SlaveState{
+    SELECTED,
+    TRANSFERING,
+    POST_TRANSFER,
+    RESET
+};
+SlaveState state = SlaveState::RESET;
 
 // SPI slave example
 static uint8_t rx_buffer[64];
 static uint8_t tx_buffer[64];
-static uint32_t select_state = 0x00;
-static uint32_t transfer_state = 0x00;
 
 void onTransferFinished() {
-    transfer_state = 1;
+    state = SlaveState::POST_TRANSFER;
 }
 
-void onSelect(uint8_t state) {
-    if (state)
-        select_state = state;
+void onSelect(uint8_t is_selected) {
+    if( SlaveState::TRANSFERING == state ){
+        if( ! is_selected ){
+            // TODO: Inform user.
+            SPI.transferCancel();
+            state = SlaveState::POST_TRANSFER; // If Master pulls SS down then reset.
+            return;
+        }
+    }
+    
+    if (is_selected) {
+        state = SlaveState::SELECTED;
+    } else {
+        state = SlaveState::RESET; // If Master pulls SS down then reset.
+    }
+}
+
+void rx_buffer_to_serial(){
+    Serial.printf("Received %d bytes", SPI.available());
+    Serial.println();
+    for (int i = 0; i < SPI.available(); i++) {
+        Serial.printf("%02x ", rx_buffer[i]);
+    }
+    Serial.println();
+}
+
+void rx_buffer_to_publish(){
+    Particle.publish("spi_rx_len",
+        String::format("Received %d bytes", SPI.available())
+        );
+    String received;
+    received.reserve(64+1);
+    for (int i = 0; i < SPI.available(); i++) {
+        received += char(rx_buffer[i]);
+        // received += String::format("%02x ", rx_buffer[i]);
+    }
+    Particle.publish("spi_rx_asci", received);
+    // Particle.publish("spi_rx_data", received);
 }
 
 /* executes once at startup */
@@ -24,47 +65,40 @@ void setup() {
 #ifdef SERIAL_EN
     Serial.begin(9600);
 #endif
-    for (int i = 0; i < sizeof(tx_buffer); i++){
-      tx_buffer[i] = (uint8_t)i;
+    for (unsigned i = 0; i < sizeof(tx_buffer); i++){
+        tx_buffer[i] = (uint8_t)i;
     }
     SPI.onSelect(onSelect);
     SPI.begin(SPI_MODE_SLAVE, A2);
+    pinMode(D7, OUTPUT);
 }
 
 /* executes continuously after setup() runs */
 void loop() {
-    while (1) {
-        while(select_state == 0){};
-        select_state = 0;
+    switch(state) {
+        default:
+        case SlaveState::RESET:
+        case SlaveState::TRANSFERING:
+            return;
 
-        transfer_state = 0;
-        SPI.transfer(tx_buffer, rx_buffer, sizeof(rx_buffer), onTransferFinished);
-        while(transfer_state == 0){};
-        if (SPI.available() > 0) {
+        case SlaveState::SELECTED:
+            digitalWrite(D7, 1);
+            SPI.transfer(tx_buffer, rx_buffer, sizeof(rx_buffer), onTransferFinished);
+            state = SlaveState::TRANSFERING;
+            break;
             
+        case SlaveState::POST_TRANSFER:
+            if (SPI.available() > 0) {
 #ifdef PUBLISH_EN
-            Particle.publish("spi_rx_len",
-                String::format("Received %d bytes", SPI.available())
-                );
-            String received;
-            received.reserve(64+1);
-            for (int i = 0; i < SPI.available(); i++) {
-                received += char(rx_buffer[i]);
-                // received += String::format("%02x ", rx_buffer[i]);
-            }
-            Particle.publish("spi_rx_asci", received);
-            // Particle.publish("spi_rx_data", received);
+                rx_buffer_to_publish();
 #endif
-
 #ifdef SERIAL_EN
-            Serial.printf("Received %d bytes", SPI.available());
-            Serial.println();
-            for (int i = 0; i < SPI.available(); i++) {
-                Serial.printf("%02x ", rx_buffer[i]);
-            }
-            Serial.println();
+                rx_buffer_to_serial();
 #endif
-
-        }
+            break;
+            }
+            
+            state = SlaveState::RESET;
+            digitalWrite(D7, 0);
     }
 }
